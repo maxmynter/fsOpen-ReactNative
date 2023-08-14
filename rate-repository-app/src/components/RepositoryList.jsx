050fbfab8f450554f6328ff71be5c2edf85c7e74
Max Mynter <maxmynter@me.com>
2023-08-14 17:23:02 +0200

    Ex. 10.23

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -1,6 +1,8 @@
 import { FlatList, View, StyleSheet, Pressable } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import { useNavigate } from "react-router-native";
 import RepositoryItem from "./RepositoryItem";
+import Text from "./Text";
 
 import useRepositories from "../hooks/useRepositories";
 
@@ -8,21 +10,112 @@ const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  listHeaderPickerHeader: {
+    height: 20,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listHeaderOpenPicker: {
+    minHeight: 40,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listHeaderOpenPickerText: {
+    fontSize: 20,
+  },
+  pickerStyle: { paddingTop: 3 },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
+const ListHeader = ({
+  selectedOrdering,
+  setSelectedOrdering,
+  sortingOptions,
+}) => {
+  const [open, setOpen] = useState(false);
+  const onChangeOrdering = (itemValue) => {
+    console.log("Changed Ordering to", itemValue);
+    setSelectedOrdering(
+      sortingOptions.find((item) => item.value === itemValue)
+    );
+    setOpen(false);
+  };
+  return (
+    <View>
+      {open ? (
+        <View>
+          <View style={styles.listHeaderPickerHeader}>
+            <Text style={styles.listHeaderOpenPickerText}>Select Ordering</Text>
+          </View>
+          <Picker
+            onValueChange={onChangeOrdering}
+            selectedValue={selectedOrdering.value}
+            styles={styles.pickerStyle}
+          >
+            {sortingOptions.map((itm, idx) => (
+              <Picker.Item key={idx} label={itm.label} value={itm.value} />
+            ))}
+          </Picker>
+        </View>
+      ) : (
+        <View style={styles.listHeaderOpenPicker}>
+          <Pressable
+            onPress={() => {
+              setOpen(true);
+            }}
+          >
+            <Text style={styles.listHeaderOpenPickerText}>
+              {selectedOrdering.label}
+            </Text>
+          </Pressable>
+        </View>
+      )}
+    </View>
+  );
+};
+
 export const RepositoryListContainer = ({ repositories }) => {
+  const sortingOptions = [
+    { label: "Latest", value: "latest" },
+    { label: "Rating Increasing", value: "rating-lth" },
+    { label: "Rating Decreasing", value: "rating-htl" },
+  ];
+  const [selectedOrdering, setSelectedOrdering] = useState(sortingOptions[0]);
   const navigate = useNavigate();
   // Get the nodes from the edges array
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
 
+  const sortedData = () => {
+    switch (selectedOrdering.value) {
+      case "latest":
+        return repositoryNodes;
+      case "rating-lth":
+        return repositoryNodes.sort(
+          (a, b) => a.ratingAverage - b.ratingAverage
+        );
+      case "rating-htl":
+        return repositoryNodes.sort(
+          (a, b) => b.ratingAverage - a.ratingAverage
+        );
+    }
+  };
+
   return (
     <FlatList
-      data={repositoryNodes}
+      data={sortedData()}
       ItemSeparatorComponent={ItemSeparator}
+      ListHeaderComponent={
+        <ListHeader
+          selectedOrdering={selectedOrdering}
+          setSelectedOrdering={setSelectedOrdering}
+          sortingOptions={sortingOptions}
+        />
+      }
       renderItem={({ item }) => (
         <Pressable
           onPress={() => {
